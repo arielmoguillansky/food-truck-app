ff21938148879666674751da0b33cbbe9d7d1236
kemba <ariel.moguillansky@42i.co>
2024-07-04 23:57:00 -0300

    added markers, styles

--- foodtruck-ui/pages/index.js
+++ foodtruck-ui/pages/index.js
@@ -1,118 +1,160 @@
+import React, { useEffect, useState, useRef, useCallback } from "react";
+import {
+  Map,
+  AdvancedMarker,
+  useMap,
+  useAdvancedMarkerRef,
+} from "@vis.gl/react-google-maps";
+import { MarkerClusterer } from "@googlemaps/markerclusterer";
 import Image from "next/image";
-import { Inter } from "next/font/google";
+import TruckIcon from "@/src/icons/food-pin.svg";
+import GPSIcon from "@/src/icons/gps.svg";
 
-const inter = Inter({ subsets: ["latin"] });
+const SF_BOUNDS = {
+  north: 37.929811,
+  south: 37.640314,
+  west: -122.59307,
+  east: -122.281479,
+};
 
-export default function Home() {
+export async function getStaticProps() {
+  const res = await fetch(
+    `${process.env.DATA_SERVER_HOST}${process.env.DATA_SERVER_URL}`
+  );
+  const repo = await res.json();
+  const parseData = repo
+    .map((truck) => ({
+      id: truck.objectid,
+      key: truck.applicant,
+      location: {
+        lat: parseFloat(truck.location.latitude),
+        lng: parseFloat(truck.location.longitude),
+      },
+    }))
+    .filter(
+      (value, index, self) =>
+        index === self.findIndex((t) => t.key === value.key)
+    );
+  return { props: { parseData } };
+}
+
+const ZOOM_MAP = 19;
+const DEFAULT_LOCATION = { lat: 37.7749, lng: -122.4194 };
+
+export default function Page({ parseData }) {
+  const map = useMap();
+  const clusterer = useRef(null);
+  const [userLocation, setUserLocation] = useState(DEFAULT_LOCATION);
+  const [data, setData] = useState([]);
+  const [selectedPlace, setSelectedPlace] = useState(null);
+  const [markerRef, marker] = useAdvancedMarkerRef();
+
+  const getUserLocation = () => {
+    if (navigator.geolocation) {
+      console.log("Geolocation is supported");
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          console.log({ lat: latitude, lng: longitude });
+          setUserLocation({ lat: latitude, lng: longitude });
+        },
+        (error) => {
+          console.error("Error getting user location:", error);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  };
+
+  useEffect(() => {
+    if (!map) return;
+    if (!clusterer.current) {
+      clusterer.current = new MarkerClusterer({ map });
+    }
+  }, [map]);
+
+  const handleClick = useCallback((ev) => {
+    if (!map) return;
+    if (!ev.latLng) return;
+    console.log("marker clicked:", ev);
+    map.panTo(ev.latLng);
+  });
+
+  const handleBounds = () => {
+    const aNord = map.getBounds().getNorthEast().lat();
+    const aEst = map.getBounds().getNorthEast().lng();
+    const aSud = map.getBounds().getSouthWest().lat();
+    const aWest = map.getBounds().getSouthWest().lng();
+    const getLocs = parseData.filter(
+      (truck) =>
+        truck.location.lat < aNord &&
+        truck.location.lat > aSud &&
+        truck.location.lng < aEst &&
+        truck.location.lng > aWest
+    );
+    setData(getLocs);
+  };
+
+  const handleTruckDetail = (loc) => {
+    map.panTo({ lat: loc.location.lat, lng: loc.location.lng });
+  };
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="relative w-screen h-screen overflow-hidden">
+      <button
+        onClick={getUserLocation}
+        className="absolute z-10 w-12 h-12 bg-red-500 rounded-full bottom-2 right-2"
+      >
+        <Image src={GPSIcon} width={60} height={60} />
+      </button>
+      <div className="relative flex">
+        <div className="absolute z-10 w-1/4 h-[calc(100%-24px)] card-header overflow-hidden bg-white rounded-t-3xl  top-4 left-4">
+          <div className="flex flex-col items-center p-4 ">
+            <h1 className="text-3xl">SanFran</h1>
+            <h1 className="text-4xl">Eats</h1>
+          </div>
+          <div className="pb-4 overflow-y-auto h-[calc(100%-108px)]">
+            {data.map((loc) => (
+              <div
+                key={loc.id}
+                className="flex items-center justify-start p-4 border-b-2 border-gray-300 cursor-pointer"
+                onClick={() => handleTruckDetail(loc)}
+              >
+                <span>{loc.key}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="w-full h-screen">
+          <Map
+            style={{ width: "100%", height: "100%" }}
+            defaultCenter={DEFAULT_LOCATION}
+            defaultZoom={ZOOM_MAP}
+            gestureHandling={"greedy"}
+            disableDefaultUI={true}
+            mapId={process.env.MAP_ID}
+            minZoom={5}
+            maxZoom={19}
+            onCameraChanged={handleBounds}
+            restriction={{
+              latLngBounds: SF_BOUNDS,
+              strictBounds: true,
+            }}
+          />
+          {data.map((poi) => (
+            <AdvancedMarker
+              ref={markerRef}
+              key={poi.id}
+              position={poi.location}
+              clickable={true}
+              onClick={handleClick}
+              collisionBehavior="OPTIONAL_AND_HIDES_LOWER_PRIORITY"
+            >
+              <Image width={60} height={60} src={TruckIcon} alt={poi.key} />
+            </AdvancedMarker>
+          ))}
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
